624be0db4212c1c09282b2ea80dba5b75ad6c1d6
HakimBouali <146171916+HakimBouali@users.noreply.github.com>
2023-10-19 09:04:47 +0200

    test

---
+++ test-FizzBuz.py
@@ -0,0 +1,16 @@
+import unittest
+from FizzBuzz import *
+
+class TestFizzBuzz(unittest.TestCase):
+    def setUp(self):
+         self.instance=FizzBuzz()
+
+    def test_affiche_deux_param(self):
+         nombre1 = 10
+         nombre2 = 16
+         self.assertEqual(self.instance.affiche(nombre1, nombre2), "Buzz11Fizz1314FrisBee16")
+    
+
+
+if __name__ == '__main__':
+     unittest.main()

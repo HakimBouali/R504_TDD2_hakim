2b7283b54ad02bb595e2acb6dfd61c6fa237fb96
HakimBouali <146171916+HakimBouali@users.noreply.github.com>
2023-10-19 09:03:46 +0200

    FizzBuz

---
+++ FizzBuz.py
@@ -0,0 +1,15 @@
+class FizzBuzz():
+    def affiche(self, n1, n2):
+        liste = ""
+        for i in range(n1, n2+1):
+            if i % 15 == 0:
+                liste += "f"
+            elif i % 3 == 0:
+                liste += "Fizz"
+            elif i % 5 == 0:
+                liste += "Buzz"
+            else:
+                liste += str(i)
+        print(liste)
+        return liste
+
